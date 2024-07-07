1a50ebb74066c04607dc672754b4793b021e6b27
Raghunandan <raghunandanmali1157@gmail.com>
2024-07-08 02:39:14 +0530

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Raghunandan Mali on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
